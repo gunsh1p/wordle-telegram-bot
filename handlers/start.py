fdc54e5ac19237f4de6c0e57db63497d8c972a5e
Бертолло Виктор <bertollo.victor@gmail.com>
2023-08-24 23:30:42 +0300

    Update in main branch, yes

--- handlers/start.py
+++ handlers/start.py
@@ -1,12 +1,14 @@
-import os
-
+from aiogram import Router, F
 from aiogram import Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message
-from aiogram import types
 
 from states import *
 
+router = Router() 
+
+@router.message(Command("start"))
 async def start(message: Message, state: FSMContext):
     if await state.get_state() == Play.running and await state.get_data()["step"] < 6:
         await message.answer("Your are lose!")
@@ -15,12 +17,12 @@ async def start(message: Message, state: FSMContext):
     await state.clear()
     kb = [
         [
-            types.KeyboardButton(text="Play"),
-            types.KeyboardButton(text="My results")
+            KeyboardButton(text="Play"),
+            KeyboardButton(text="My results")
         ]
     ]
-    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+    keyboard = ReplyKeyboardMarkup(keyboard=kb)
     await message.answer(f"Hello, {message.from_user.first_name}. It's a wordle game for {'tg_channel_name_will_be_soon'}", reply_markup=keyboard)
 
-def register_start(dp: Dispatcher):
-    dp.register_message_handler(start, commands=["start"], state="*")
+def register_router(dp: Dispatcher):
+    dp.include_router(router)
